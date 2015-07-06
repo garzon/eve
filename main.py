f37100edb229441ef6f45275119578cc22f65534
garzon <610204679@qq.com>
2015-07-06 10:27:05 +0800

    bug-free

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ def query(solarSys):
 	neighbor = garzonEve.SolarSystemArray.fromIDSet(solarSys.getNeighborInJumps(4)).add(solarSys['sysID'])
 	neighbor = neighbor.filter(lambda sys: sys.isNotSoSafe() and sum(sys['jumpsHist'][-6*3:]) == 0)
 	if neighbor.count() < 3: return False
-	print neighbor[neighbor.map.keys()[0]]['jumpsHist']
 	return True
 
 
 allsys = garzonEve.SolarSystemArray.all()
 res = allsys.filter(query)
-# print res
+print res
